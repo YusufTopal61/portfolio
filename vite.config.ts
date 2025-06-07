1656fe38a3f7e4c7993f6f7c2d211a9856893cce
Yusuf Topal <yusuf.topal@hva.nl>
2025-06-07 21:36:00 +0200

    root base

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
@@ -6,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',   // <<<<<<<<<< hier je subpath toevoegen
   server: {
     host: "::",
     port: 8080,
