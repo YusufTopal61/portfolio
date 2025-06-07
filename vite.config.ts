0caa6d2d076d1d7d1f5872e5d1055190231787dd
Yusuf Topal <yusuf.topal@hva.nl>
2025-06-07 23:08:23 +0200

    trying to fix no show fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/portfolio/',
+
   server: {
-    base: '/portfolio/',
     host: "::",
     port: 8080,
   },
   plugins: [
     vue(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
