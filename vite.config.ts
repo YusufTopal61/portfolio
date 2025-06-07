9876eb704e93914d9e21b7af634efa94b14647a9
Yusuf Topal <yusuf.topal@hva.nl>
2025-06-07 21:41:51 +0200

    updated footer

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',   // <<<<<<<<<< hier je subpath toevoegen
+  base: '/yusuftopal61/',   // <<<<<<<<<< hier je subpath toevoegen
   server: {
     host: "::",
     port: 8080,
