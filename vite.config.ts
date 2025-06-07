dcd1b1573680c8fa4b78edb811c15a764f358a1e
Yusuf Topal <yusuf.topal@hva.nl>
2025-06-07 23:05:33 +0200

    trying to fix no show fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base: '/portfolio/',
     host: "::",
     port: 8080,
   },
